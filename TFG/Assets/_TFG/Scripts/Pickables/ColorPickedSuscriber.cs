4dcbfa8e6088ed0e858fe72df0176d72ccd8d1fe
G J  .P <petergeorghe.julca@enti.cat>
2023-01-30 03:59:35 +0100

    PowerUps Affecting Player Properly - ToDo Abilities Update

--- TFG/Assets/_TFG/Scripts/Pickables/ColorPickedSuscriber.cs
+++ TFG/Assets/_TFG/Scripts/Pickables/ColorPickedSuscriber.cs
@@ -14,8 +14,10 @@ public class ColorPickedSuscriber : MonoBehaviour
     void Start()
     {
         characterMaterials = GetComponentsInChildren<Renderer>();
-        ColorPickupEvents colorPickedEvent = GetComponent<ColorPickupEvents>();
-        colorPickedEvent.onColorPicked += ColorPickedEvent_OnColorPicked;
+        ColorPickupEvents colorPickedEvents = GetComponent<ColorPickupEvents>();
+        colorPickedEvents.onColorBluePicked += ColorPickedEvent_OnColorPickedBlue;
+        colorPickedEvents.onColorRedPicked += ColorPickedEvent_OnColorPickedRed;
+        colorPickedEvents.onColorGreenPicked += ColorPickedEvent_OnColorPickedGreen;
     }
 
     void ChangeMaterialSettings(int index)
@@ -29,13 +31,36 @@ public class ColorPickedSuscriber : MonoBehaviour
         }
     }
 
-    private void ColorPickedEvent_OnColorPicked(object sender, EventArgs e)
+    //Blue
+    private void ColorPickedEvent_OnColorPickedBlue(object sender, EventArgs e)
     {
-        Debug.Log("Color Picked!");
+        Debug.Log("Color Picked Blue!");
+        ChangeMaterialSettings(0);
+        //Example If The Script Is In Another GameObject
+        //ColorPickupEvents colorPickupEvents = (ColorPickupEvents)sender;
+        ColorPickupEvents colorPickedEvents = GetComponent<ColorPickupEvents>();
+        colorPickedEvents.onColorBluePicked -= ColorPickedEvent_OnColorPickedBlue;
+    }
+
+    //Red
+    private void ColorPickedEvent_OnColorPickedRed(object sender, EventArgs e)
+    {
+        Debug.Log("Color Picked Red!");
         ChangeMaterialSettings(1);
         //Example If The Script Is In Another GameObject
         //ColorPickupEvents colorPickupEvents = (ColorPickupEvents)sender;
-        ColorPickupEvents colorPickupEvents = GetComponent<ColorPickupEvents>();
-        colorPickupEvents.onColorPicked -= ColorPickedEvent_OnColorPicked;
+        ColorPickupEvents colorPickedEvents = GetComponent<ColorPickupEvents>();
+        colorPickedEvents.onColorRedPicked += ColorPickedEvent_OnColorPickedRed;
+    }
+
+    //Green
+    private void ColorPickedEvent_OnColorPickedGreen(object sender, EventArgs e)
+    {
+        Debug.Log("Color Picked Green!");
+        ChangeMaterialSettings(2);
+        //Example If The Script Is In Another GameObject
+        //ColorPickupEvents colorPickupEvents = (ColorPickupEvents)sender;
+        ColorPickupEvents colorPickedEvents = GetComponent<ColorPickupEvents>();
+        colorPickedEvents.onColorGreenPicked += ColorPickedEvent_OnColorPickedGreen;
     }
 }
--- TFG/Assets/_TFG/Scripts/Pickables/ColorPickupEvents.cs
+++ TFG/Assets/_TFG/Scripts/Pickables/ColorPickupEvents.cs
@@ -6,36 +6,18 @@ using UnityEngine;
 public class ColorPickupEvents : MonoBehaviour
 {
     public event EventHandler onColorTouched;
-    public event EventHandler onColorPicked;
+    public event EventHandler onColorBluePicked;
+    public event EventHandler onColorRedPicked;
+    public event EventHandler onColorGreenPicked;
 
     public GameObject ColorTest;
     public GameObject BodyLayer;
 
-    private void Start()
-    {
-        onColorTouched += Testing_OnColorTouched;
-    }
-
-    private void Testing_OnColorTouched(object sender, EventArgs e)
-    {
-        Debug.Log("Color Touched: ");
-    }
-
-    /*void ChangeMaterialSettings(int index)
-    {
-        for (int i = 0; i < characterMaterials.Length; i++)
-        {
-            if (characterMaterials[i].transform.CompareTag("PlayerHorns"))
-                characterMaterials[i].material.SetColor("_EmissionColor", hornColors[index]);
-            else
-                characterMaterials[i].material.SetTexture("_MainTex", albedoList[index]);
-        }
-    }*/
-
     //Update Testings
     private void Update()
     {
-        /*if (Input.GetKeyDown(KeyCode.Space))
+        /* Testings
+         * if (Input.GetKeyDown(KeyCode.Space))
         {
             onColorPicked?.Invoke(this, EventArgs.Empty);
         }
@@ -45,8 +27,10 @@ public class ColorPickupEvents : MonoBehaviour
             ColorTest.gameObject.SetActive(true);
         }*/
     }
+    
     //Find A Proper Way Of Doing This Once The Project Is Accepted!!!!!
-    private void OnTriggerEnter(Collider other)
+    /*Original
+     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Pick_Color"))
         {
@@ -56,6 +40,41 @@ public class ColorPickupEvents : MonoBehaviour
             BodyLayer.layer = other.gameObject.layer;
             Debug.Log("Collided With Pickeable");
             onColorPicked?.Invoke(this, EventArgs.Empty);
+            other.gameObject.SetActive(false);
+        }
+        else
+        {
+            Debug.Log("Collided With Random");
+        }
+    }
+     */
+
+    private void LayerChanger(Collider other, string color)
+    {
+        Debug.Log(BodyLayer.layer);
+        Debug.Log(other.gameObject.layer);
+        Debug.Log("End Of Debug " + color);
+        BodyLayer.layer = other.gameObject.layer;
+        Debug.Log("Collided With Pickeable");
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick_Color_Blue"))
+        {
+            LayerChanger(other, "BLUE");
+            onColorBluePicked?.Invoke(this, EventArgs.Empty);
+            other.gameObject.SetActive(false);
+        }
+        else if (other.gameObject.CompareTag("Pick_Color_Red"))
+        {
+            LayerChanger(other, "RED");
+            onColorRedPicked?.Invoke(this, EventArgs.Empty);
+            other.gameObject.SetActive(false);
+        }
+        else if (other.gameObject.CompareTag("Pick_Color_Green"))
+        {
+            LayerChanger(other, "GREEN");
+            onColorGreenPicked?.Invoke(this, EventArgs.Empty);
             other.gameObject.SetActive(false);
         }
         else
