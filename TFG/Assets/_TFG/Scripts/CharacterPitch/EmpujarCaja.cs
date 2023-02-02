754c8308977008e8d05fd9c240bd1acb746bd8a8
Eduard Rosell <eduard.rosell@enti.cat>
2023-02-02 21:08:30 +0100

    MoveBox

--- TFG/Assets/_TFG/Scripts/CharacterPitch/EmpujarCaja.cs
+++ TFG/Assets/_TFG/Scripts/CharacterPitch/EmpujarCaja.cs
@@ -5,15 +5,42 @@ using UnityEngine;
 public class EmpujarCaja : MonoBehaviour
 {
     public float pushPower = 2.0f;
+
+    bool estaActivo;
     // Start is called before the first frame update
     void Start()
     {
-        
+     
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        estaActivo = true;
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (estaActivo == true)
+        {
+            Rigidbody body = hit.collider.attachedRigidbody;
+
+            if (body == null || body.isKinematic)
+            {
+                return;
+            }
+            if (hit.moveDirection.y < -0.3)
+            {
+                return;
+            }
+
+            Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
+
+            body.velocity = pushDir * pushPower;
+        }
+        else
+        {
+            estaActivo = false;
+        }
     }
 }
