bb0fcdb05685427a11b87a742cfb85711e732faa
Eduard Rosell <eduard.rosell@enti.cat>
2023-02-02 20:26:56 +0100

    CheckpointFailed
    Muy fail

--- TFG/Assets/_TFG/Scripts/CharacterPitch/Checkpoint.cs
+++ TFG/Assets/_TFG/Scripts/CharacterPitch/Checkpoint.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEditor;
 public class Checkpoint : MonoBehaviour
 {
     [SerializeField] GameObject player;
@@ -9,25 +9,47 @@ public class Checkpoint : MonoBehaviour
 
     [SerializeField] Vector3 vectorPoint;
 
-    [SerializeField] float dead;
+    [SerializeField] float dead = 0.5f;
+    bool isDead = false;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(player.transform.position.y < -dead)
+        //Debug.Log("VectorPoint: " + vectorPoint);
+        if (Input.GetKeyDown(KeyCode.O))
         {
-            player.transform.position = vectorPoint;
+            isDead = true;
+            //Debug.Log("Key Pressed");
         }
+        if (isDead == true)
+        {
+            Debug.Log("TP: " + vectorPoint);
+                player.transform.position = vectorPoint;
+                //player.transform.position = checkPoints.transform.position;
+                isDead = false;
+            //EditorApplication.isPaused = true;
+
+        }
+       
     }
 
+   /* IEnumerator waitForDeath()
+    {
+        yield return new WaitForSeconds(dead);
+        Debug.Log("Wait");
+       
+    }*/
     private void OnTriggerEnter(Collider other)
     {
-        vectorPoint = other.transform.position;
-        Destroy(other.gameObject);
+        if (other.CompareTag("Checkpoint"))
+        {
+            vectorPoint = other.gameObject.transform.position;
+        }
+        
+        //Destroy(other.gameObject);
     }
 }
