0474c0466987bed966a397667dc0336285a6e3d4
Eduard Rosell <eduard.rosell@enti.cat>
2023-02-20 17:55:45 +0100

    MoverCaja_TotalmenteFuncional

--- TFG/Assets/_TFG/Scripts/CharacterPitch/EmpujarCaja.cs
+++ TFG/Assets/_TFG/Scripts/CharacterPitch/EmpujarCaja.cs
@@ -7,11 +7,10 @@ public class EmpujarCaja : MonoBehaviour
 {
     public float pushPower = 2.0f;
 
-    bool estaActivo;
     // Start is called before the first frame update
     void Start()
     {
-        estaActivo = true;
+      
     }
 
     // Update is called once per frame
@@ -25,44 +24,29 @@ public class EmpujarCaja : MonoBehaviour
 
     private void ToggleBoxActive()
     {
-        if (estaActivo == true)
-            estaActivo = false;
-        else if (estaActivo == false)
-            estaActivo = true;
+       
     }
 
     private void OnControllerColliderHit(ControllerColliderHit hit)
     {
-        Rigidbody body = hit.collider.attachedRigidbody;
-        if (estaActivo)
-        {
-            if (body == null || body.isKinematic)
-            {
-                return;
-            }
-            if (hit.moveDirection.y < -0.3)
-            {
-                return;
-            }
-            Debug.Log("Empujando");
-            Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
+        if(this.enabled){ 
+            Rigidbody body = hit.collider.attachedRigidbody;
+            
+            
+                if (body == null || body.isKinematic)
+                {
+                    return;
+                }
 
-            body.velocity = pushDir * pushPower;
+                if (hit.moveDirection.y < -0.3)
+                {
+                    return;
+                }
+
+                Debug.Log("Empujando");
+                Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
+
+                body.velocity = pushDir * pushPower;
         }
-        else if (!estaActivo)
-        {
-            if (body == null || body.isKinematic)
-            {
-                return;
-            }
-            if (hit.moveDirection.y < -0.3)
-            {
-                return;
-            }
-            Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
-
-            body.velocity = pushDir * 0;
-        }
-
     }
 }
