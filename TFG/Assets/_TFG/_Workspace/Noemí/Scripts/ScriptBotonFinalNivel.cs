764a9a9c45d60c292b00a9c07234d9f277b39bec
Georghe Julca <petergeorghe.julca@enti.cat>
2023-03-20 08:28:57 +0100

    Lightning Profile: Tutorial Scene

--- TFG/Assets/_TFG/_Workspace/Noemí/Scripts/ScriptBotonFinalNivel.cs
+++ TFG/Assets/_TFG/_Workspace/Noemí/Scripts/ScriptBotonFinalNivel.cs
@@ -8,6 +8,7 @@ public class ScriptBotonFinalNivel : MonoBehaviour
     public GameObject Boton;
     public ParticleSystem confeti;
     public Material botonActivado;
+    public float delay;
     [SerializeField] string _nextScene;
 
     void OnCollisionEnter(Collision collision)
@@ -22,7 +23,7 @@ public class ScriptBotonFinalNivel : MonoBehaviour
 
     private IEnumerator LoadNextScene(string levelName)
     {
-        yield return new WaitForSeconds(0.3f);
+        yield return new WaitForSeconds(delay);
         SceneManager.LoadScene(levelName);
     }
 }
